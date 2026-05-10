120ad1fe3e0cf2113cf6453570ecb31c53095489
rin-rin0141 <168decade@gmail.com>
2026-05-10 11:44:52 +0900

    Fix DEBUG config

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["nagoyameshi-2.herokuapp.com", "nagoyameshi-2-b161d8950f42.herokuapp.com"]
 
