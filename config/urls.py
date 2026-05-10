7d6ded2d3de265a69b71d8c862572e0c789a0bab
rin-rin0141 <168decade@gmail.com>
2026-05-10 17:50:48 +0900

    Serve media files in production temporarily

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from base.views import Top_displayView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     # Adminサイト
@@ -24,3 +25,9 @@ urlpatterns = [
 # 開発環境時のみ/media/... にアクセスが来たら、MEDIA_ROOT の中を見せる
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
